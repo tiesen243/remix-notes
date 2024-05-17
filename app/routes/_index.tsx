87dae3e3da7f3a0a61696d363a084a1b909a2dc3
Tiesen <ttien56906@gmail.com>
2024-05-18 04:23:51 +0700

    feat: init project

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,91 @@
-import type { MetaFunction } from "@remix-run/node";
+import { ActionFunctionArgs, json } from "@remix-run/node";
+import { Form, useActionData, useLoaderData } from "@remix-run/react";
+import { z } from "zod";
 
-export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { db } from "@/lib/db";
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+const schema = z.object({
+  title: z.string().min(1, "Title must be at least 1 character"),
+  content: z.string().min(1, "Content must be at least 1 character"),
+});
+
+export async function action({ request }: ActionFunctionArgs) {
+  if (request.method === "DELETE") {
+    const id = (await request.formData()).get("id");
+    if (!id) return json({ message: "Post not found!" }, { status: 404 });
+    await db.post.delete({ where: { id: Number(id) } });
+    return json({ message: "Post deleted!" }, { status: 200 });
+  }
+  const data = schema.safeParse(Object.fromEntries(await request.formData()));
+  if (!data.success)
+    return json(data.error.flatten().fieldErrors, { status: 400 });
+
+  await db.post.create({ data: data.data });
+  return json({ message: "Post created!" }, { status: 201 });
+}
+
+export async function loader() {
+  const posts = await db.post.findMany();
+  return json(posts);
+}
+
+const Page: React.FC = () => {
+  const errors = useActionData<typeof action>();
+  const posts = useLoaderData<typeof loader>();
+
+  return (
+    <main className="container">
+      <Form method="POST" className="space-y-4">
+        <div className="space-y-1">
+          <Label htmlFor="title">Title</Label>
+          <Input type="text" name="title" />
+          {errors && "title" in errors && (
+            <p className="text-destructive text-sm">{errors.title}</p>
+          )}
+        </div>
+
+        <div className="space-y-1">
+          <Label htmlFor="content">Content</Label>
+          <Input type="text" name="content" />
+          {errors && "content" in errors && (
+            <p className="text-destructive text-sm">{errors.content}</p>
+          )}
+        </div>
+
+        <Button>Create</Button>
+      </Form>
+
+      <ul className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        {posts.map((post) => (
+          <li key={post.id}>
+            <Card>
+              <CardHeader>
+                <CardTitle>{post.title}</CardTitle>
+                <CardDescription>{post.content}</CardDescription>
+              </CardHeader>
+
+              <CardFooter>
+                <Form method="DELETE">
+                  <input type="hidden" name="id" value={post.id} />
+                  <Button>Delete</Button>
+                </Form>
+              </CardFooter>
+            </Card>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
 };
 
-export default function Index() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
-}
+export default Page;
